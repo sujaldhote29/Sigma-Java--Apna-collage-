3de1d06708120a013e4d9b8e189c394730e93f65
Sujal <sujaldhote1@gmail.com>
2024-03-15 20:04:30 +0530

    just

--- Greedy Algorithms/Smallest_String.java
+++ Greedy Algorithms/Smallest_String.java
@@ -1,20 +1,20 @@
 public class Smallest_String {
-    public static String string(int n , int k){
+    public static String string(int n, int k) {
         int totalchar = n;
         int cursum = k;
         StringBuilder sb = new StringBuilder();
-        while (totalchar>0) {
-            if ((totalchar-1)*26>=cursum) {
+        while (totalchar > 0) {
+            if ((totalchar - 1) * 26 >= cursum) {
                 sb.append('a');
                 cursum--;
-            }else{
-                int position = cursum%26;
-                if (position==0) {
+            } else {
+                int position = cursum % 26;
+                if (position == 0) {
                     sb.append('z');
-                    cursum-=26;
-                }else{
-                    sb.append((char)(position-1+'a'));
-                    cursum-=position;
+                    cursum -= 26;
+                } else {
+                    sb.append((char) (position - 1 + 'a'));
+                    cursum -= position;
                 }
             }
             totalchar--;
@@ -22,6 +22,7 @@ public class Smallest_String {
 
         return sb.toString();
     }
+
     public static void main(String[] args) {
         int n = 5;
         int k = 42;
