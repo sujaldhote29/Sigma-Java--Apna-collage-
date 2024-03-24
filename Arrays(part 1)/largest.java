081c265975eb76ac09f3f16d3b646773ad9069c5
Sujal <sujaldhote1@gmail.com>
2024-03-24 13:44:32 +0530

    changes

--- Arrays(part 1)/largest.java
+++ Arrays(part 1)/largest.java
@@ -2,20 +2,29 @@
 
 public class largest {
     public static int larger(int num[]){
-        int large = Integer.MIN_VALUE;
-        for(int i = 0; i<num.length; i++){
-            if (large < num[i]) {
-                 large = num[i];
-            }
+        // int large = Integer.MIN_VALUE;
+        // for(int i = 0; i<num.length; i++){
+        //     if (large < num[i]) {
+        //          large = num[i];
+        //     }
+        // }
+        int large = num[0];
+        for(int i =1; i<num.length-1; i++){
+           large = Math.max(large, num[i]);
         }
         return large;
     }
     public static int smallest(int num[]){
-        int small = Integer.MAX_VALUE;
-        for(int i =0; i<num.length; i++){
-            if (small>num[i]) {
-                small = num[i];
-            }
+        // int small = Integer.MAX_VALUE;
+        // for(int i =0; i<num.length; i++){
+        //     if (small>num[i]) {
+        //         small = num[i];
+        //     }
+        // }
+
+        int small = num[0];
+        for(int i =1; i<num.length-1; i++){
+           small = Math.min(small, num[i]);
         }
         return small;
     }
